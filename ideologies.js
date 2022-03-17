2568b70a23d0aeee1fa969c9b951c64a1cd5a781
mblx1 <98678707+mblx1@users.noreply.github.com>
2022-03-17 16:56:22 +1100

    Add files via upload

--- ideologies.js
+++ ideologies.js
@@ -1,6 +1,6 @@
 ideologies = [
     {
-        "name": "Anarcho-Communism",
+        "name": "Anarcho-Communism/Communist Party",
         "stats": {
             "econ": 100,
             "dipl": 50,
@@ -9,7 +9,7 @@ ideologies = [
         }
     },
     {
-        "name": "Libertarian Communism",
+        "name": "Libertarian Communism/Communist Party",
         "stats": {
             "econ": 100,
             "dipl": 70,
@@ -18,7 +18,7 @@ ideologies = [
         }
     },
     {
-        "name": "Trotskyism",
+        "name": "Trotskyism/Communist Party",
         "stats": {
             "econ": 100,
             "dipl": 100,
@@ -27,7 +27,7 @@ ideologies = [
         }
     },
     {
-        "name": "Marxism",
+        "name": "Marxism-Leninism/Communist Party",
         "stats": {
             "econ": 100,
             "dipl": 70,
@@ -36,7 +36,7 @@ ideologies = [
         }
     },
     {
-        "name": "De Leonism",
+        "name": "De Leonism/Communist Party",
         "stats": {
             "econ": 100,
             "dipl": 30,
@@ -45,7 +45,7 @@ ideologies = [
         }
     },
     {
-        "name": "Leninism",
+        "name": "Leninism/Communist Party",
         "stats": {
             "econ": 100,
             "dipl": 40,
@@ -54,7 +54,7 @@ ideologies = [
         }
     },
     {
-        "name": "Stalinism/Maoism",
+        "name": "Stalinism/Communist Party",
         "stats": {
             "econ": 100,
             "dipl": 20,
@@ -180,7 +180,7 @@ ideologies = [
         }
     },
     {
-        "name": "Social Democracy",
+        "name": "Social Democracy/Democratic Progressive Party",
         "stats": {
             "econ": 60,
             "dipl": 70,
@@ -189,7 +189,7 @@ ideologies = [
         }
     },
     {
-        "name": "Progressivism",
+        "name": "Progressivism/Democratic Progressive Party",
         "stats": {
             "econ": 60,
             "dipl": 80,
@@ -369,7 +369,7 @@ ideologies = [
         }
     },
     {
-        "name": "Classical Liberalism",
+        "name": "Classical Liberalism/Libertarian Right Party",
         "stats": {
             "econ": 30,
             "dipl": 60,
@@ -378,7 +378,7 @@ ideologies = [
         }
     },
     {
-        "name": "Authoritarian Capitalism",
+        "name": "Authoritarian Capitalism/Patriot Party",
         "stats": {
             "econ": 20,
             "dipl": 30,
@@ -396,7 +396,7 @@ ideologies = [
         }
     },
     {
-        "name": "Neo-Conservatism",
+        "name": "Neo-Conservatism/Patriot Party",
         "stats": {
             "econ": 20,
             "dipl": 20,
@@ -414,7 +414,7 @@ ideologies = [
         }
     },
     {
-        "name": "Libertarian Capitalism",
+        "name": "Libertarian Capitalism/Libertarian Right",
         "stats": {
             "econ": 20,
             "dipl": 50,
@@ -423,7 +423,7 @@ ideologies = [
         }
     },
     {
-        "name": "Market Anarchism",
+        "name": "Market Anarchism/Libertarian Right",
         "stats": {
             "econ": 20,
             "dipl": 50,
@@ -432,7 +432,7 @@ ideologies = [
         }
     },
     {
-        "name": "Objectivism",
+        "name": "Objectivism/Libertarian Right",
         "stats": {
             "econ": 10,
             "dipl": 50,
@@ -459,7 +459,7 @@ ideologies = [
         }
     },
     {
-        "name": "Anarcho-Capitalism",
+        "name": "Anarcho-Capitalism/Libertarian Right",
         "stats": {
             "econ": 0,
             "dipl": 50,
@@ -490,10 +490,165 @@ ideologies = [
         "stats": {
             "econ": 100,
             "dipl": 40,
-            "govt": 60,
+            "govt": 30,
             "scty": 90
         }
     },
-   
+    {
+    "name": "New Libertarian Centre Party",
+        "stats": {
+            "econ": 50,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Unionist Party",
+            "stats": {
+                "econ": 80,
+                "dipl": 80,
+                "govt": 80,
+                "scty": 80
+            }
+        },
+        {
+            "name": "Patriotic Socialist Party",
+                "stats": {
+                    "econ": 67,
+                    "dipl": 35,
+                    "govt": 20,
+                    "scty": 35
+                }
+            },
+            {
+                "name": "Socialist Nationalist Party",
+                    "stats": {
+                        "econ": 70,
+                        "dipl": 20,
+                        "govt": 15,
+                        "scty": 30
+                    }
+                },
+                {
+                "name": "u/TheBasedMBLX",
+                "stats": {
+                    "econ": 17,
+                    "dipl": 37,
+                    "govt": 72,
+                    "scty": 30
+                }
+            },
+            {
+                "name": "English Socialism",
+                "stats": {
+                    "econ": 50,
+                    "dipl": 0,
+                    "govt": 0,
+                    "scty": 0
+                }
+            },
+            {
+                "name": "u/OrganizationNo3213",
+                "stats": {
+                    "econ": 79,
+                    "dipl": 72,
+                    "govt": 65,
+                    "scty": 70
+                }
+            },
+            {
+                "name": "u/CornyPunz1",
+                "stats": {
+                    "econ": 69,
+                    "dipl": 46,
+                    "govt": 80,
+                    "scty": 62
+                }
+            },
+            {
+                "name": "u/Mackyboy41",
+                "stats": {
+                    "econ": 36,
+                    "dipl": 35,
+                    "govt": 30,
+                    "scty": 35
+                }
+            },
+            {
+                "name": "u/Leftandright161616",
+                "stats": {
+                    "econ": 62,
+                    "dipl": 19,
+                    "govt": 10,
+                    "scty": 24
+                }
+            },
+            {
+                "name": "u/Callemannen007",
+                "stats": {
+                    "econ": 36,
+                    "dipl": 60,
+                    "govt": 64,
+                    "scty": 60
+                }
+            },
+            {
+                "name": "u/xrc1808",
+                "stats": {
+                    "econ": 36,
+                    "dipl": 30,
+                    "govt": 21,
+                    "scty": 30
+                }
+            },
+            {
+                "name": "u/lost_mah_account",
+                "stats": {
+                    "econ": 83.3,
+                    "dipl": 50,
+                    "govt": 50,
+                    "scty": 70
+                }
+            },
+            {
+                "name": "u/theleningradcowboy",
+                "stats": {
+                    "econ": 93,
+                    "dipl": 87,
+                    "govt": 84,
+                    "scty": 95
+                }
+            },
+            {
+                "name": "u/vicandmath",
+                "stats": {
+                    "econ": 21,
+                    "dipl": 33,
+                    "govt": 28,
+                    "scty": 16
+                }
+            },
+            {
+                "name": "u/kingofthewombat",
+                "stats": {
+                    "econ": 64,
+                    "dipl": 60,
+                    "govt": 56,
+                    "scty": 73
+                }
+            },
+            {
+                "name": "u/AdministrationMain94",
+                "stats": {
+                    "econ": 16,
+                    "dipl": 18,
+                    "govt": 77,
+                    "scty": 23
+                }
+            },
+                
+                
+
 
     ];
